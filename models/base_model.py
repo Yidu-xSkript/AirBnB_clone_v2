08a7ff3c7a88f988b69847f626cf35d906961d08
Yidu-xSkript <yidu95@gmail.com>
2023-01-05 11:05:45 +0300

    File Storage updated (Delete functionality added -> base_model) + create function in console updated to add items based on parameters (key=value) + added mysql setups for dev and test

--- models/base_model.py
+++ models/base_model.py
@@ -2,43 +2,47 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
-
+import models
 
 class BaseModel:
     """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+        """Instantiates a new model"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+        dateToStr = "%Y-%m-%dT%H:%M:%S.%f"
+
+        if kwargs:
+            # self.__dict__.update(kwargs)
+            for k, v in kwargs.items():
+                if (k == "created_at" or k == "updated_at"):
+                    v = datetime.strptime(v, dateToStr)
+                if k != "__class__":
+                    setattr(self, k, v)
 
     def __str__(self):
         """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        _dict = self.__dict__.copy()
+        _dict.pop("_sa_instance_state", None)
+        return '[{}] ({}) {}'.format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        from models import storage
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
-        dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
-        return dictionary
+        _dict = self.__dict__.copy()
+        _dict["__class__"] = self.__class__.__name__
+        _dict["updated_at"] = self.updated_at.isoformat()
+        _dict["created_at"] = self.created_at.isoformat()
+        _dict.pop("_sa_instance_state", None)
+        return _dict
+
+    def delete(self):
+        """Delete the current instance from storage."""
+        models.storage.delete(self)
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,50 +1,61 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
-
+from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, _class=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if _class is not None:
+            if type(_class) == str:
+                _class = eval(_class)
+            _class_dict = {}
+            for k, v in self.__objects.items():
+                if type(v) == _class:
+                    _class_dict[k] = v
+            return _class_dict
+        return self.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
-        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+        self.__objects[obj.__class__.__name__ + '.' + obj.__dict__['id']] = obj
 
     def save(self):
         """Saves storage dictionary to file"""
-        with open(FileStorage.__file_path, 'w') as f:
-            temp = {}
-            temp.update(FileStorage.__objects)
-            for key, val in temp.items():
-                temp[key] = val.to_dict()
-            json.dump(temp, f)
+        to_JSON = {obj: self.__objects[obj].to_dict()
+                   for obj in self.__objects.keys()}
+        with open(self.__file_path, 'w') as fp:
+            json.dump(to_JSON, fp)
 
     def reload(self):
         """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
-
-        classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
         try:
-            temp = {}
-            with open(FileStorage.__file_path, 'r') as f:
-                temp = json.load(f)
-                for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+            with open(self.__file_path, "r") as fp:
+                _dict = json.load(fp)
+                for o in _dict.values():
+                    className = o["__class__"]
+                    del o["__class__"]
+                    self.new(eval(className)(**o))
         except FileNotFoundError:
+            return
+
+    def delete(self, obj=None):
+        """Delete a given object from __objects, if it exists."""
+        try:
+            del self.__objects["{}.{}".format(type(obj).__name__, obj.id)]
+        except (AttributeError, KeyError):
             pass
+
+    def close(self):
+        """Call the reload method."""
+        self.reload()
