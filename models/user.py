01f6415d7ae5fde4542111de73cc77f19702d6dd
Yidu-xSkript <yidu95@gmail.com>
2023-01-06 12:30:58 +0300

    db storage - places -> updated | city + user updated relationship with places

--- models/user.py
+++ models/user.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel
 from models.base_model import Base
 from sqlalchemy import Column
 from sqlalchemy import String
+from sqlalchemy.orm import relationship
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
@@ -12,3 +13,4 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
+    places = relationship("Place", backref="user", cascade="delete")
